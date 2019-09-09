6b4c2072756657e242d3640b427e3743ccdd84ea
McPerry_ <mack.perry.co@gmail.com>
2019-09-09 23:56:09 +0800

    V0.2.0 - Setup Raffle Views (Incomplete)

--- resources/views/_layout.blade.php
+++ resources/views/_layout.blade.php
@@ -4,12 +4,19 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta name="csrf-token" content="{{ csrf_token() }}">
-	<title>Alumni Registraiton</title>
+	<title>Raffle</title>
 	@include('_styles')
 	@yield('styles')
 </head>
 <body>
-	
+	<div class="container-fluid">
+		<div class="row justify-content-center">
+			<div class="col-lg-11 p-0">
+				@yield('body')
+			</div>
+		</div>
+	</div>
+
 	@include('_scripts')
 	@yield('scripts')
 </body>
--- resources/views/raffle.blade.php
+++ resources/views/raffle.blade.php
@@ -1,5 +1,16 @@
 @extends('_layout')
+@section('styles')
+<link rel="stylesheet" href="{{ asset('css/raffle.css') }}">
+@endsection
 
 @section('body')
-
+<div class="container-fluid">
+	<div class="row justify-content-center">
+		<div class="col-lg-7 vh-100">
+			<div class="mt-lg-5" id="reels">
+				Hello World
+			</div>
+		</div>
+	</div>
+</div>
 @endsection
