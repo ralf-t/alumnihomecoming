cd8740f8d0c8f1048ab13674d09b8492b45cbe47
kayerosacia <chayegt@gmail.com>
2019-09-10 14:39:54 +0800

    V0.3.0 - Updated raffle page (Incomplete)

--- resources/views/raffle.blade.php
+++ resources/views/raffle.blade.php
@@ -6,9 +6,13 @@
 @section('body')
 <div class="container-fluid">
 	<div class="row justify-content-center">
-		<div class="col-lg-7 vh-100">
-			<div class="mt-lg-5" id="reels">
-				Hello World
+		<div class="col-lg-5 vh-100">
+			<div id="reels">
+				<!-- <canvas id="digit1"></canvas>
+				<canvas id="digit2"></canvas>
+				<canvas id="digit3"></canvas>
+				<canvas id="digit4"></canvas> -->
+				<div id="winline"></div>
 			</div>
 		</div>
 	</div>
